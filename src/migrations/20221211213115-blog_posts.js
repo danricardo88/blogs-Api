fab71dad53f263ac5e8791ddbcd22846fa6bc150
danricardo88 <daniel.ricardo88@outlook.com>
2022-12-11 19:40:02 -0300

    req-01

---
+++ src/migrations/20221211213115-blog_posts.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      
+      title: { type: Sequelize.STRING },
+      content: { type: Sequelize.STRING },
+      
+      user_id: { allowNull: false, onUpDate: 'CASCADE', onDelete: 'CASCADE', type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+      },
+
+      published: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+      updated: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
